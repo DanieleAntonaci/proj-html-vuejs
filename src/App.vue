6cc8615acec0503a4c3f991b5ba8a117621c3545
DanieleAntonaci <danieleanto96@gmail.com>
2022-12-21 11:19:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import Footer from './components/Footer.vue';
+import Header from './components/Header.vue'
+import Main from './components/Main.vue';
+export default {
+  components: {
+    Header,
+    Main,
+    Footer
+  }
+}
+</script>
+
+<template>
+  <Header/>
+
+  <Main/>
+
+  <Footer/>
+</template>
+
+<style lang="scss">
+@use './style/general.scss' as *;
+@use './style/partials/mixin' as *;
+@use './style/partials/variables' as *;
+</style>
